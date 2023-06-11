8b9d3fed3b75cf40f67a2e14136c0a1e78391ca7
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-06-11 22:49:15 +0300

    Initial commit

---
+++ src/test/java/ru/netology/Test/MySqlTest.java
@@ -0,0 +1,51 @@
+package ru.netology.Test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.Data.DataHelper;
+import ru.netology.Data.SQLHelper;
+import ru.netology.Page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.Data.SQLHelper.cleanDatabase;
+
+public class MySqlTest {
+
+    @AfterAll
+    static void tearDown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.VerifyErrorNotificationVisibility();
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with exist in base and active user random verification")
+    void shouldGetErrorNotificationFLoginWithExistUserANdRandomVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisiblity();
+    }
+}
